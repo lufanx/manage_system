425290a2a24a81db8264cc6e6452ef8ce2960267
lu fan <lux.fan@intel.com>
2017-12-14 20:34:06 +0800

    Log: add for manage_system log file handle.
    log.h and log.c current only ERROR system handle.
    
    Signed-off-by: lu fan <lux.fan@intel.com>

---
+++ include/log.h
@@ -0,0 +1,17 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+enum manage_system_log_level{
+	MANAGE_SYSTEM_LOG_ERROR,
+	MANAGE_SYSTEM_LOG_WARN,
+	MANAGE_SYSTEM_LOG_NOTICE,
+};
+
+/* Define manage_system log handle function  */
+#define LOG_ERROR(...) \
+		manage_system_log(MANAGE_SYSTEM_LOG_ERROR, __FILE__, __LINE__, __func__, __VA_ARGS__)
+
+void manage_system_log(enum manage_system_log_level, const char *file,
+			   	const int line, const char *func, const char *format, ...);
+
+#endif /* __LOG_H__ end  */
---
+++ src/log.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <syslog.h>
+
+#include "log.h"
+#include "common_handle.h"
+
+enum manage_system_log_level printf_manage_system_log = MANAGE_SYSTEM_LOG_NOTICE;
+
+static const char *const log_level_names[] = {
+	[MANAGE_SYSTEM_LOG_ERROR]	= "ERROR",
+	[MANAGE_SYSTEM_LOG_WARN]	= "WARNING",
+	[MANAGE_SYSTEM_LOG_NOTICE]	= "NOTICE",
+};
+
+/* Handle manage_system_log  */
+void
+manage_system_log(enum manage_system_log_level level, const char *file, const int line, const char *func,
+				 const char *format, ...)
+{
+	char buf[VA_ARGS_BUF];
+	va_list ap;
+	char severity = LOG_INFO;
+
+	switch (level) {
+	case MANAGE_SYSTEM_LOG_ERROR:
+		severity = LOG_ERR;
+		break;
+	default:
+		fprintf(stderr, "manage_system_log_level error\n");
+		break;
+	}
+
+	va_start(ap, format);
+	vsnprintf(buf, sizeof(buf), format, ap);
+
+	openlog("MANAGE_SYSTEM", LOG_PERROR, LOG_LOCAL7);
+
+	/* If level is error and warn will output  */
+	if (level <= printf_manage_system_log) {
+		syslog(severity, "%s:%4d:%s: *%s*: %s", file, line, func, log_level_names[level], buf);
+	}
+	
+	closelog();
+
+	va_end(ap);
+}
